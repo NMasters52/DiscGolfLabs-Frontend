c41a10869846aadf2ab2f6120bbb127a6996ff78
nmasters <nmasters52@gmail.com>
2026-01-12 13:19:08 -0500

    init

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/_index/index.jsx"),
+  route("sign-in", "routes/auth/sign-in.jsx"),
+  route("sign-up", "routes/auth/sign-up.jsx"),
+  route("app", "routes/app/_layout.jsx", [
+    index("routes/app/_index.jsx"),
+    route("dashboard", "routes/app/dashboard/index.jsx"),
+  ]),
+] satisfies RouteConfig;
